dc45a9cd4974e66f5ccf28a1a2696b0f984333dd
aron kolyanga <kolarony9@gmail.com>
2022-11-08 12:40:03 +0300

    update

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ createApp({
 			message: "Hello Vue!",
 		};
 	},
+	methods: {
+		tvon() {
+			return this.tvon;
+		},
+		tvoff() {
+			return this.tvoff;
+		},
+		tvnext() {
+			return this.tvnext;
+		},
+		tvprevsion() {
+			return this.tvprevsion;
+		},
+	},
 }).mount("#app");
