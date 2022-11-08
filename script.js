ba3cb1db3523e9cb6eb53838f0498f3b4b32dd0f
TrippleA-Ashaba <trippleaunit@hotmail.com>
2022-11-08 15:49:57 +0300

    almost final tv

--- script.js
+++ script.js
@@ -3,9 +3,33 @@ const { createApp } = Vue;
 createApp({
 	data() {
 		return {
+			on: false,
 			message: "Looper VueTv!",
-			channel: "https://i.gifer.com/9dDE.gif",
-			on: true,
+			channels: [
+				"https://i.gifer.com/2of.gif",
+				"https://i.gifer.com/NHBl.gif",
+				"https://i.gifer.com/9dDE.gif",
+			],
+			visible: 0,
 		};
+	},
+	methods: {
+		onOff() {
+			this.on = !this.on;
+		},
+		next() {
+			if (this.visible >= this.channels.length - 1) {
+				this.visible = 0;
+			} else {
+				this.visible++;
+			}
+		},
+		prev() {
+			if (this.visible <= 0) {
+				this.visible = this.channels.length - 1;
+			} else {
+				this.visible--;
+			}
+		},
 	},
 }).mount("#app");
